2d0491719893696bcb76252dd06fa490c9a5ba41
michbur <michalburdukiewicz@gmail.com>
2018-05-05 22:28:06 -0400

    testing motifs

---
+++ motif-test/get_motif.R
@@ -0,0 +1,19 @@
+library(dplyr)
+
+aas <- toupper(biogram:::return_elements("prot"))
+
+raw_motif1 <- gsub("[[:space:]]", "", "X - KPR") %>% 
+  strsplit("") %>% 
+  unlist
+
+if("-" %in% raw_motif1) {
+  
+  lhs <- raw_motif1[1L:(which(raw_motif1 == "-") - 1)]
+  rhs <- raw_motif1[(which(raw_motif1 == "-") +1):length(raw_motif1)]
+  
+  if(grepl("X", lhs)) {
+    lhs <- aas
+  }
+  
+  setdiff(lhs, rhs)
+}
