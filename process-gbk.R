926d245808e8c3f571f40456cd34161f5616a53a
michbur <michalburdukiewicz@gmail.com>
2018-05-10 12:18:39 -0400

    fixed process_gbk

--- process-gbk.R
+++ process-gbk.R
@@ -7,17 +7,18 @@ source("./SMS/functions.R")
 
 #awk -v n=1 '/^$/{close("out"n);n++;next} {print > "/home/michal/Dropbox/ann-arbor-collab/gut-microbiom-data/out"n}' 42773.ADKP01000001-ADKP01000236.nuc.gbk
 
-res <- pblapply(list.files("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-data", full.names = TRUE)[1L:5], function(file_name)
-  try({
-    dat <- data.frame(file = last(strsplit(x = file_name, split = "/", fixed = TRUE)[[1]]),
-                      suppressMessages(readGenBank2(file_name)), stringsAsFactors = FALSE)
-    }, silent = TRUE)
+res <- pblapply(list.files("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-data", full.names = TRUE), 
+                function(file_name)
+                  try({
+                    dat <- data.frame(file = last(strsplit(x = file_name, split = "/", fixed = TRUE)[[1]]),
+                                      suppressMessages(readGenBank2(file_name)), stringsAsFactors = FALSE)
+                  }, silent = TRUE)
 )
 
 save(res, file = "/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
-load("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
-
-res_df <- bind_rows(res[sapply(res, function(i) class(i) %in% "data.frame")])
-
-seqinr::write.fasta(sequences = strsplit(res_df[["seq"]], ""), names = res_df[["protein_id"]],
-                    file = "/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/raw_seq.fasta")
+# load("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
+# 
+# res_df <- bind_rows(res[sapply(res, function(i) class(i) %in% "data.frame")])
+# 
+# seqinr::write.fasta(sequences = strsplit(res_df[["translation"]], ""), names = res_df[["protein_id"]],
+#                     file = "/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/raw_seq.fasta")
