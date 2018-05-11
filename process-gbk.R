6c05ac96faeac33fb1cf1bbf324b3e5b3e30f4a5
michbur <michalburdukiewicz@gmail.com>
2018-05-10 22:09:00 -0400

    splitting them

--- process-gbk.R
+++ process-gbk.R
@@ -16,9 +16,16 @@ res <- pblapply(list.files("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome
 )
 
 save(res, file = "/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
-# load("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
-# 
-# res_df <- bind_rows(res[sapply(res, function(i) class(i) %in% "data.frame")])
-# 
-# seqinr::write.fasta(sequences = strsplit(res_df[["translation"]], ""), names = res_df[["protein_id"]],
-#                     file = "/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/raw_seq.fasta")
+
+load("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/Gastrointestinal_tract.RData")
+
+res_df <- bind_rows(res[sapply(res, function(i) class(i) %in% "data.frame")])
+
+seq_splits <- split(1L:nrow(res_df), ceiling(quantile(1L:nrow(res_df), 1L:20/20)))
+
+lapply(1L:length(seq_splits), function(ith_split) {
+  seqinr::write.fasta(sequences = strsplit(res_df[seq_splits[[ith_split]], "translation"], ""), 
+                      names = res_df[seq_splits[[ith_split]], "protein_id"],
+                      file = paste0("/home/michal/Dropbox/ann-arbor-collab/gut-microbiome-results/sequences/split", 
+                                    ith_split, ".fasta"))
+})
